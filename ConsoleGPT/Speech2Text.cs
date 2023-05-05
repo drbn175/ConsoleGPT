6ebdebd81c89eeacfa5c0c4f91a349879f58680f
David Betancourt <david.betancourt@agilethought.com>
2023-05-05 15:36:14 -0600

    Added voice recognition

---
+++ ConsoleGPT/Speech2Text.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGPT
+{
+    public static class Speech2Text
+    {
+        public static async Task<string> RecognizeSpeechAsync(string subscriptionKey, string serviceRegion, string language)
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key // and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription(subscriptionKey, serviceRegion);
+
+            // Creates a speech recognizer.
+            //Language in BCP-47
+            using (var recognizer = new SpeechRecognizer(config,SourceLanguageConfig.FromLanguage(language)))
+            {
+                Console.WriteLine("Listening...");
+                var text = string.Empty;
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result. 
+                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                // shot recognition like command or query. 
+                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync();
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    text = result.Text;
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+
+                return text;
+            }
+        }
+    }
+}
